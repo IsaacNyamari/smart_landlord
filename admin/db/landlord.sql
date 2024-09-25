25eb86d4f763af777a334f38cd6dcab6707b6d8d
Isaac Nyamari <jablessions76@gmail.com>
2024-09-25 13:50:31 +0300

    add

---
+++ admin/db/landlord.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 25, 2024 at 12:48 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `landlord`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activation`
+--
+
+CREATE TABLE `activation` (
+  `code_id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `activation`
+--
+
+INSERT INTO `activation` (`code_id`, `email`, `code`) VALUES
+(4, 'jablessions76@gmail.com', 'C27C96');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apartments`
+--
+
+CREATE TABLE `apartments` (
+  `apart_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `location` text DEFAULT NULL,
+  `caretaker` text DEFAULT NULL,
+  `rooms` text NOT NULL,
+  `vacant` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `caretakers`
+--
+
+CREATE TABLE `caretakers` (
+  `caretaker_id` int(11) NOT NULL,
+  `names` text NOT NULL,
+  `id_number` text NOT NULL,
+  `phone` text NOT NULL,
+  `employee` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owners`
+--
+
+CREATE TABLE `owners` (
+  `owner_id` varchar(60) NOT NULL,
+  `fname` text NOT NULL,
+  `lname` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `owners`
+--
+
+INSERT INTO `owners` (`owner_id`, `fname`, `lname`, `email`, `password`, `status`) VALUES
+('f9698a216fd0f18fa1c97c24d6673e4d', 'Isaac', 'Nyamari', 'jablessions76@gmail.com', '$2y$10$WvJD9FTkx2iooVAYdn7T2.EGieDsBGma0TZXU4foeg7a/BawjaMlC', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenants`
+--
+
+CREATE TABLE `tenants` (
+  `tenant_id` int(11) NOT NULL,
+  `id_number` text NOT NULL,
+  `names` text DEFAULT NULL,
+  `apartment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activation`
+--
+ALTER TABLE `activation`
+  ADD PRIMARY KEY (`code_id`);
+
+--
+-- Indexes for table `apartments`
+--
+ALTER TABLE `apartments`
+  ADD PRIMARY KEY (`apart_id`);
+
+--
+-- Indexes for table `caretakers`
+--
+ALTER TABLE `caretakers`
+  ADD PRIMARY KEY (`caretaker_id`);
+
+--
+-- Indexes for table `owners`
+--
+ALTER TABLE `owners`
+  ADD PRIMARY KEY (`owner_id`),
+  ADD UNIQUE KEY `email` (`email`) USING HASH;
+
+--
+-- Indexes for table `tenants`
+--
+ALTER TABLE `tenants`
+  ADD PRIMARY KEY (`tenant_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activation`
+--
+ALTER TABLE `activation`
+  MODIFY `code_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `apartments`
+--
+ALTER TABLE `apartments`
+  MODIFY `apart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `caretakers`
+--
+ALTER TABLE `caretakers`
+  MODIFY `caretaker_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tenants`
+--
+ALTER TABLE `tenants`
+  MODIFY `tenant_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
