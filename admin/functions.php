f90ac4f98e08d65a898ef31b5e9236a395cea8e9
Isaac Nyamari <jablessions76@gmail.com>
2024-09-25 15:45:01 +0300

    Initial commit

---
+++ admin/functions.php
@@ -0,0 +1,92 @@
+<?php
+class Db
+{
+    protected $hostname = "localhost";
+    protected $username = "root";
+    protected $password = "";
+    protected $database = "landlord";
+
+    protected function connect()
+    {
+        $conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->database) or die(mysqli_connect_error());
+        if ($conn) {
+            $data = $conn;
+            return $data;
+        }
+    }
+}
+class Log extends Db
+{
+    public $connection;
+
+    public function __construct()
+    {
+        $this->connection = $this->connect();
+    }
+}
+class Caretakers extends Db
+{
+    public $connection;
+
+    public function getAparts() {}
+    public function addApart()
+    {
+        $this->connection = $this->connect();
+    }
+    public function deleteApart()
+    {
+        $this->connection = $this->connect();
+    }
+    public function updateApart()
+    {
+        $this->connection = $this->connect();
+    }
+}
+class Apartments extends Db
+{
+    public $connection;
+
+    public function getAparts()
+    {
+        $this->connection = $this->connect();
+        $query = "SELECT * FROM apartments";
+        $result = mysqli_query($this->connect(), $query);
+        return $result;
+    }
+    public function addApart()
+    {
+        $this->connection = $this->connect();
+    }
+    public function deleteApart()
+    {
+        $this->connection = $this->connect();
+    }
+    public function updateApart()
+    {
+        $this->connection = $this->connect();
+    }
+}
+class Tenants extends Db
+{
+    public $connection;
+
+    public function getTenants()
+    {
+        $this->connection = $this->connect();
+    }
+    public function addTenant()
+    {
+        $this->connection = $this->connect();
+    }
+    public function deleteTenant()
+    {
+        $this->connection = $this->connect();
+    }
+    public function updateTenant()
+    {
+        $this->connection = $this->connect();
+    }
+}
+$getAparts = new Apartments;
+$get_apartments = $getAparts->getAparts();
+var_dump(($get_apartments));
