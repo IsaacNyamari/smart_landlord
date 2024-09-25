861081b86dfb17ec73c121bca14c66e42e7c442a
Isaac Nyamari <jablessions76@gmail.com>
2024-09-25 17:37:20 +0300

    Initial commit

--- admin/index.php
+++ admin/index.php
@@ -1,163 +1,190 @@
-<?php include("header.php")?>
-  <!-- ======= Sidebar ======= -->
-  <?php include("side_bar.php")?>
-  <!-- End Sidebar-->
+<?php
+require("protect.php");
+include("functions.php");
+$email = $_SESSION['email'];
+$owner = $getOwners->getOwner(trim($email));
+$status = $owner['status'];
 
-  <main id="main" class="main">
+include("header.php") ?>
+<!-- ======= Sidebar ======= -->
+<?php include("side_bar.php");
+if ($status == 0) {
+  echo "<div style='
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: #fad81a;
+    color: white;
+    text-align: center;
+    padding: 15px;
+    font-size: 18px;
+    z-index: 9999;
+'>
+    ⚠️ Your account is not activated!
+    <a href='../activate.php' style='color: white; text-decoration: underline;'>Activate Here</a>
+</div>
+";
+}
 
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+?>
+<!-- End Sidebar-->
 
-    <section class="section dashboard">
-      <div class="row">
+<main id="main" class="main">
 
-        <!-- Left side columns -->
-        <div class="col-lg-8">
-          <div class="row">
+  <div class="pagetitle">
+    <h1>Dashboard</h1>
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active">Dashboard</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
 
-            <!-- Sales Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card sales-card">
+  <section class="section dashboard">
+    <div class="row">
 
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
+      <!-- Left side columns -->
+      <div class="col-lg-8">
+        <div class="row">
 
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
+          <!-- Sales Card -->
+          <div class="col-xxl-4 col-md-6">
+            <div class="card info-card sales-card">
+
+              <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title">Rent Collected <span>| This Month</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-currency-dollar"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>Kshs. 150,000</h6>
+                    <span class="text-success small pt-1 fw-bold">92%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+
+                  </div>
                 </div>
+              </div>
 
-                <div class="card-body">
-                  <h5 class="card-title">Rent Collected <span>| This Month</span></h5>
+            </div>
+          </div><!-- End Sales Card -->
 
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>Kshs. 150,000</h6>
-                      <span class="text-success small pt-1 fw-bold">92%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+          <!-- Revenue Card -->
+          <div class="col-xxl-4 col-md-6">
+            <div class="card info-card revenue-card">
 
-                    </div>
+              <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title">Revenue <span>| This Year</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-currency-dollar"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>Kshs. 10m</h6>
+                    <span class="text-success small pt-1 fw-bold">88%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div><!-- End Revenue Card -->
+
+          <!-- Tenants Card -->
+          <div class="col-xxl-4 col-md-6">
+
+            <div class="card info-card customers-card">
+
+              <div class="filter">
+                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                  <li class="dropdown-header text-start">
+                    <h6>Filter</h6>
+                  </li>
+
+                  <li><a class="dropdown-item" href="#">Today</a></li>
+                  <li><a class="dropdown-item" href="#">This Month</a></li>
+                  <li><a class="dropdown-item" href="#">This Year</a></li>
+                </ul>
+              </div>
+
+              <div class="card-body">
+                <h5 class="card-title">Tenants <span>| This Year</span></h5>
+
+                <div class="d-flex align-items-center">
+                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                    <i class="bi bi-people"></i>
+                  </div>
+                  <div class="ps-3">
+                    <h6>124</h6>
+                    <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
+
                   </div>
                 </div>
 
               </div>
-            </div><!-- End Sales Card -->
+            </div>
 
-            <!-- Revenue Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card revenue-card">
+          </div><!-- End Customers Card -->
 
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
+          <!-- Reports -->
+          <?php include("report_section.php") ?>
+          <!-- End Reports -->
 
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
+          <!-- Recent Sales -->
 
-                <div class="card-body">
-                  <h5 class="card-title">Revenue <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>Kshs. 10m</h6>
-                      <span class="text-success small pt-1 fw-bold">88%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Revenue Card -->
-
-            <!-- Tenants Card -->
-            <div class="col-xxl-4 col-md-6">
-
-              <div class="card info-card customers-card">
-
-                <div class="filter">
-                  <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                    <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
-                    </li>
-
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                  </ul>
-                </div>
-
-                <div class="card-body">
-                  <h5 class="card-title">Tenants <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>124</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-
-            </div><!-- End Customers Card -->
-
-            <!-- Reports -->
-            <?php include("report_section.php")?>
-            <!-- End Reports -->
-            
-            <!-- Recent Sales -->
-            
-            <?php include("recent_sales.php")?>
-            <!-- End Recent Sales -->
+          <?php include("recent_sales.php") ?>
+          <!-- End Recent Sales -->
 
 
-          </div>
-        </div><!-- End Left side columns -->
+        </div>
+      </div><!-- End Left side columns -->
 
-        <!-- Right side columns -->
-        <div class="col-lg-4">
-          <!-- Budget Report -->
-          <!-- <?php include('budget_report.php')?> -->
-          <!-- End Budget Report -->
+      <!-- Right side columns -->
+      <div class="col-lg-4">
+        <!-- Budget Report -->
+        <!-- <?php include('budget_report.php') ?> -->
+        <!-- End Budget Report -->
 
-          <!-- Website Traffic -->
-          <?php include("web_traffic.php")?>
-          <!-- End Website Traffic -->
+        <!-- Website Traffic -->
+        <?php include("web_traffic.php") ?>
+        <!-- End Website Traffic -->
 
-        </div><!-- End Right side columns -->
+      </div><!-- End Right side columns -->
 
-      </div>
-    </section>
+    </div>
+  </section>
 
-  </main><!-- End #main -->
+</main><!-- End #main -->
 
 
-<?php include("footer.php")?>
+<?php include("footer.php") ?>
