c9606007a8a8748e175dc31f94e423bd766a49ba
Isaac Nyamari <jablessions76@gmail.com>
2024-09-25 14:58:09 +0300

    edits

---
+++ admin/dashboard_nav.php
@@ -0,0 +1,18 @@
+<li class="nav-item">
+    <a class="nav-link" href="./">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="messages">
+        <i class="bi bi-chat"></i>
+        <span>Messsages</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="reports">
+        <i class="bi bi-book"></i>
+        <span>Reports</span>
+    </a>
+</li>
---
+++ admin/side_bar.php
@@ -0,0 +1,47 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <?php include("dashboard_nav.php") ?>
+        <!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Manage</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="tenants">
+                        <i class="bi bi-circle"></i><span>Tenants</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="caretakers">
+                        <i class="bi bi-circle"></i><span>Caretaker</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="apartments">
+                        <i class="bi bi-circle"></i><span>Apartments</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Tables Nav -->
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="users-profile.php">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="logout.php">
+                <i class="bi bi-box-arrow-in-right text-danger"></i>
+                <span>Log Out</span>
+            </a>
+        </li><!-- End Login Page Nav -->
+    </ul>
+
+</aside>
