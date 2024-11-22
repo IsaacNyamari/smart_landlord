5de9be8258aa4c5f4b2bd304fc5f676c7303a38e
Isaac Nyamari <jablessions76@gmail.com>
2024-11-22 21:15:03 +0300

    initial commit

--- management/header.php
+++ management/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-
+include('protect.php');
 include("loader.php");
 include("../admin/initialize.php");
 ?>
---
+++ management/login.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../pico-main/css/pico.min.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <main>
+        <form action="#">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="password">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+            <button name="submit">Login</button>
+        </form>
+    </main>
+</body>
+
+</html>
